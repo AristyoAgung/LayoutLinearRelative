538c5224de11b1ebf23f10f95a9b2196b50786ca
AristyoAgung <aristyo.agung@gmail.com>
2019-09-06 18:45:04 +0700

    tambah

--- app/src/main/java/com/e/layoutlinearrelative/MainActivity.java
+++ app/src/main/java/com/e/layoutlinearrelative/MainActivity.java
@@ -2,14 +2,39 @@ package com.e.layoutlinearrelative;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_linear);
-        setContentView(R.layout.layout_relative);
+        setContentView(R.layout.activity_main);
+
+        View.OnClickListener tableButtonListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,RelativeLayoutActivity.class);
+                startActivity(intent);
+            }
+        };
+        Button tableBtn = (Button)findViewById(R.id.buttonRelative);
+        tableBtn.setOnClickListener(tableButtonListener);
+        View.OnClickListener proteinButtonListener = new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        };
+        Button proteinBtn = (Button)findViewById(R.id.buttonLinear);
+        proteinBtn.setOnClickListener(proteinButtonListener);
+        /*setContentView(R.layout.layout_linear);
+        setContentView(R.layout.layout_relative);*/
     }
 }
